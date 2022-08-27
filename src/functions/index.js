b83868c80a9c25d99618d5877bea5aad378d9622
Septian Wibisono <asep.gelo@gmail.com>
2022-08-27 22:32:28 +0700

    test write one file

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,3 +1,6 @@
 export default (req, res) => {
-  res.status(200).send(`pong`);
+  fs = require("fs");
+  fs.writeFile("cacheone.txt", "this is cache file.", 'utf8', function(err) {
+    res.status(200).send(`file written.`);
+  })
 }
