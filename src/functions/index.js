e88bbe5bc81b2c5f21f1b725cc74bdd4d32a49f2
Septian Wibisono <asep.gelo@gmail.com>
2022-08-27 22:44:59 +0700

    put failover.

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,7 +1,8 @@
 export default (req, res) => {
   fs = require("fs");
-  fs.writeFile("cacheone.txt", "this is cache file.", 'utf8', function(err) {
+  fs.writeFile("/tmp/cacheone.txt", "this is cache file.", 'utf8', function(err) {
     if (err) {
+      res.status(500).send(`fail desperately.`);
       console.log(err);
     } else {
       res.status(200).send(`file written.`);
--- src/functions/read.js
+++ src/functions/read.js
@@ -1,8 +1,9 @@
 export default (req, res) => {
   fs = require("fs");
-  fs.readFile("cacheone.txt", function(err, data) {
+  fs.readFile("/tmp/cacheone.txt", function(err, data) {
     if (err) {
       console.log(err);
+      res.status(500).send('fail desperately.');
     } else {
       res.status(200).send(data);
     }
